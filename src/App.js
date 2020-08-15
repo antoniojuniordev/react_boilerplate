1ce577a8f80769e2ead22ad3f11acb1ac34d7b07
Junior <junior4g4@gmail.com>
2020-08-15 15:17:18 -0300

    feat: adicionado suporte ao tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Routes from './routes'
+import './styles/main.css'
 
 function App () {
   return (
--- src/pages/authUser/Login.js
+++ src/pages/authUser/Login.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Login () {
   return (
-    <>
-      <h1>Login</h1>
-    </>
+    <div class='h-64'>
+      <div class='p-4 m-4 bg-green-600'>
+        <h1 class='text-2xl font-bold text-white'>Login</h1>
+      </div>
+    </div>
   )
 }
