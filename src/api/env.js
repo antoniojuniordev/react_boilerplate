588efe4d01ed8beb962a1a5f8c1c0b70f19bddf1
Junior <junior4g4@gmail.com>
2022-02-14 20:17:37 -0300

    feat: uptade react

--- src/api/env.js
+++
@@ -1,6 +0,0 @@
-export default {
-  baseURL:
-    process.env.NODE_ENV === 'production'
-      ? 'http://loclahost/api'
-      : 'http://loclahost/api'
-}
--- src/api/helpers/handlerPromisses/index.js
+++
@@ -1,6 +0,0 @@
-import { trackPromise } from 'react-promise-tracker'
-
-export function interceptPromise (promise, reference) {
-  if (reference) return trackPromise(promise, reference)
-  return trackPromise(promise)
-}
